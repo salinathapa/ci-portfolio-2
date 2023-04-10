d774376ca4fd03310e9fdf1cd96892cf45c8c9ec
Salina Thapa <salinathapa.business@hotmail.com>
2023-04-10 20:59:41 +0100

    add function js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,23 @@
 // Wait for the DOM to finish loading before running script
-document.addEventListener("DOMContentLoaded", function() {
+var expenseRowTemplate;
 
-    
+function OnAddExpenseClicked() {
+    var newElement = document.createElement("div");
+    newElement.innerHTML = expenseRowTemplate;
+    newElement.classList.add("expense-row");
+
+    document.querySelector("#expenses").appendChild(newElement);
+
+    UpdateAddExpenseBtnEvent();
+}
+
+function UpdateAddExpenseBtnEvent() {
+    document.querySelectorAll(".add-expense").forEach(function(el) {
+        el.onclick = function () {OnAddExpenseClicked()};
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    expenseRowTemplate = document.querySelector(".expense-row").innerHTML;
+    UpdateAddExpenseBtnEvent();
+});
